7d37b8ec0e101b7b8055691cd46e51862bbcb645
Jon Buttram <jbuttramDev@gmail.com>
2021-04-19 20:49:49 -0400

    express server not finding js files

--- app.js
+++ app.js
@@ -2,13 +2,31 @@
 //go to <localhost:3000> in browser
 
 const express = require("express")
-
+const multer = require("multer") //* allows localhost file upload
+const upload = multer({
+    dest: 'uploads/' //*this saves your file into a directory called "uploads"
+  }); 
 const app = express();
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+//TODO configure express
+
+//functions
+app.use('/public', express.static(__dirname + '/app/public'))
+app.use('/components', express.static(__dirname + '/app/components'))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/app/public/index.html");
 });
 
-app.listen(3000, function() {
+//run load function on submit. *req.file is the uploaded mp3
+//! local file name MUST match attributal name in html
+app.post('/', upload.single('file-to-upload'), (req, res) => {
+  res.redirect('/');
+  //!TODO fix the import file.
+  //var impFile
+  //export req.file as impFile
+});
+
+app.listen(3000, () => {
     console.log("Server is running on localhost3000")
 });
